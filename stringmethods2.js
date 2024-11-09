d59c63b475fdf61e84ddea54c64ded4de75ca648
SnigdhaReddy09 <93863995+SnigdhaReddy09@users.noreply.github.com>
2024-11-09 10:37:34 +0530

    Add files via upload

---
+++ stringmethods2.js
@@ -0,0 +1,20 @@
+// 1
+let str = "Hello World!";
+console.log(str.replace("World!","My Dear Friend"));
+
+// 2
+let a="Hungry ";
+console.log(a.repeat(10));
+
+// 3
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+let index=sentence.lastIndexOf("fox")
+console.log(index);
+
+// 4
+let message = "Hello, world!";
+console.log(message.startsWith("hi"));
+
+// 5
+let fileName = "document.pdf";
+console.log(fileName.endsWith(".pdf"));
